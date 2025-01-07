7e3e9e71669519e01af564686b2ed391603f4d75
Gayathrigeetha-Thangavel <gayathrigeetha.thangavel@gmail.com>
2025-01-07 01:34:13 +0100

    Add project files.

---
+++ Models/Machine.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_IM_Park.Models
+{
+    public class Machine
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string? MachineName { get; set; }
+
+        public string Status { get; set; } = "Offline";
+        public DateTime LastUpdatedTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ Services/MachineServices.cs
@@ -0,0 +1,46 @@
+﻿using BlazorApp_IM_Park.Models;
+
+namespace BlazorApp_IM_Park.Services
+{
+    public class MachineServices
+    {
+        private List<Machine> machinesList = new();
+
+        public MachineServices()
+        {
+            // Initialize with some dummy data
+            machinesList.Add(new Machine { MachineName = "Machine 1", Status = "Online" });
+            machinesList.Add(new Machine { MachineName = "Machine 2", Status = "Offline" });
+            machinesList.Add(new Machine { MachineName = "Machine 3", Status = "Online" });
+            machinesList.Add(new Machine { MachineName = "Machine 4", Status = "Offline" });
+        }
+
+        public Task<List<Machine>> GetMachinesAsync()
+        {
+            return Task.FromResult(machinesList);
+        }
+
+        public Task AddMachineAsync(Machine machine)
+        {
+            machinesList.Add(machine);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveMachineAsync(Guid id)
+        {
+            machinesList.RemoveAll(m => m.Id == id);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateMachineStatusAsync(Guid id, string status)
+        {
+            var machine = machinesList.FirstOrDefault(m => m.Id == id);
+            if (machine != null)
+            {
+                machine.Status = status;
+                machine.LastUpdatedTime = DateTime.Now;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
